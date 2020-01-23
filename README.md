c8e8db77429e5c03ce987dc6afc6f5b869672dcb
T.Todua <todtoma@gmail.com>
2020-01-23 21:13:39 +0400

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Generate Let's encrypt free ssl for Hostinger Shared Hosting websites.
 
-Instructions at: <a href="https://puvox.software/tools/hostinger-ssl-generator">https://puvox.software/tools/hostinger-ssl-generator</a>
+Instructions at: <a href="https://puvox.software/blog/hostinger-https-ssl-auto-install/">https://puvox.software/blog/hostinger-https-ssl-auto-install/</a>
--- auto_ssl/cron.php
+++ auto_ssl/cron.php
@@ -1,12 +1,12 @@
 <?php
-$email = 'tazotodua+ssl@gmail.com';
-$path_to_public_html = __DIR__.'/../';
-$domain = $_SERVER['HTTP_HOST']; //isset($_GET['domain']) ? $_GET['domain'] : $_SERVER['HTTP_HOST'];
-$domains = [$domain , 'www.'.$domain];
-$path_to_lescript_library = __DIR__.'/lescript-master/Lescript.php';
+$email = 'yourmail@gmail.com';				// your mail where you will get certificates
+$domain = 'yourdomain.com';					// your domain name, on which you are creating SSL's
+$path_to_public_html = __DIR__.'/../';		// path your domain public root
 
 
 // =================================== you dont belong to below ==========================================//
+$domains = [$domain , 'www.'.$domain];
+$path_to_lescript_library = __DIR__.'/lescript-master/Lescript.php';
 $cert_storate_path = __DIR__ .'/certificate/storage';
 $path_to_keys = $cert_storate_path."/$domain";
 if(!defined("PHP_VERSION_ID") || PHP_VERSION_ID < 50300 || !extension_loaded('openssl') || !extension_loaded('curl')) {
@@ -18,15 +18,26 @@ require $path_to_lescript_library;
 class Logger { function __call($name, $arguments) { echo (date('Y-m-d H:i:s')." [$name] ${arguments[0]}\n<br/>"); }}
 $logger = new Logger();
 
+function rmdir_recursive($path){
+	if(!empty($path) && is_dir($path) ){
+		$dir  = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS); //upper dirs not included,otherwise DISASTER HAPPENS :)
+		$files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::CHILD_FIRST);
+		foreach ($files as $f) {if (is_file($f)) {unlink($f);} else {$empty_dirs[] = $f;} } if (!empty($empty_dirs)) {foreach ($empty_dirs as $eachDir) {rmdir($eachDir);}} rmdir($path);
+		return true;
+	}
+	return true;
+	//include_once(ABSPATH.'/wp-admin/includes/class-wp-filesystem-base.php');
+	//\WP_Filesystem_Base::rmdir($fullPath, true);
+}
+
+
 try {
 
     $le = new Analogic\ACME\Lescript($cert_storate_path, $path_to_public_html, $logger);
     # or without $logger
-
     $le->contact = array('mailto:test@test.com'); // optional
     $le->initAccount();
     $le->signDomains($domains);
-
 	mail($email, 'SSL files for : '.$domain,  file_get_contents($path_to_keys. '/cert.pem'). PHP_EOL . PHP_EOL . file_get_contents($path_to_keys. '/private.pem'), $headers ='From: ssl_issued@'.$domain . "\r\n" .  'Reply-To: ssl_issued@'.$domain . "\r\n" .  'X-Mailer: PHP/' . phpversion() );
 	echo '<span style="font-size:2em; color:green;>SUCCESSFULLY MAILED (check your spambox)</span>';
 } catch (\Exception $e) {
@@ -36,6 +47,5 @@ try {
 finally{
 	// remove traces after processing
 	if (is_dir($path_to_keys)) rmdir_recursive($path_to_keys);
-	if (is_dir($cert_storate_path)) rmdir_recursive($cert_storate_path);
 	if (is_dir($cert_storate_path)) rmdir_recursive($cert_storate_path);
 }
