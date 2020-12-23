3a30db1ff294e1a3280b61b9cab55a32e0b2c691
T. Todua <7117978+ttodua@users.noreply.github.com>
2020-12-23 18:14:18 +0400

    Update cron.php

--- auto_ssl/cron.php
+++ auto_ssl/cron.php
@@ -2,6 +2,7 @@
 $email	='your_email@gmail.com';
 
 
+
 // ###################### core ########################
 if(!defined("PHP_VERSION_ID") || PHP_VERSION_ID < 50300 || !extension_loaded('openssl') || !extension_loaded('curl')) {
     die("You need at least PHP 5.3.0 with OpenSSL and curl extension\n");
@@ -11,11 +12,12 @@ require ($path_to_lescript_library = __DIR__.'/lescript-master/Lescript.php');
 class Logger { function __call($name, $arguments) { echo (date('Y-m-d H:i:s')." [$name] ${arguments[0]}\n<br/>"); }}  // you can use any logger according to Psr\Log\LoggerInterface
 $logger = new Logger();
 
-function rmdir_recursive($path){
-	if(!empty($path) && is_dir($path) ){
-		$dir  = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS); //upper dirs not included,otherwise DISASTER HAPPENS :)
+function rmdir_recursive($dirPath){
+	if(!empty($dirPath) && is_dir($dirPath) ){
+		$dir  = new RecursiveDirectoryIterator($dirPath, RecursiveDirectoryIterator::SKIP_DOTS); //upper dirs not included,otherwise DISASTER HAPPENS :)
 		$files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::CHILD_FIRST);
-		foreach ($files as $f) {if (is_file($f)) {unlink($f);} else {$empty_dirs[] = $f;} } if (!empty($empty_dirs)) {foreach ($empty_dirs as $eachDir) {rmdir($eachDir);}} rmdir($path);
+		foreach ($files as $path) $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname()); //{if (is_file($path)) {unlink($path);} else {$empty_dirs[] = $path;} } if (!empty($empty_dirs)) {foreach ($empty_dirs as $eachDir) {rmdir($eachDir);}} 
+		rmdir($dirPath);
 		return true;
 	}
 	return true;
@@ -25,6 +27,11 @@ function rmdir_recursive($path){
  
 
 // ###################### custom ########################
+
+$domain	= filter_var( str_replace( ['www.','\\','..'], '', $_SERVER['HTTP_HOST']), FILTER_VALIDATE_URL);
+$cert_storate_path = __DIR__ .'/certificate/storage';
+$path_to_keys = $cert_storate_path."/$domain";
+
 try { 
 	//#### check if 85 days passed ####
 	$file= __DIR__.'/tmp_last_time';
@@ -39,30 +46,27 @@ try {
 
 
 	//if (isset($argv[1])) parse_str($argv[1], $array);
-	$domain	=str_replace('www.', '', $_SERVER['HTTP_HOST']);
 	$domains = [$domain, 'www.'.$domain];
 	$path_to_public_html = $_SERVER["DOCUMENT_ROOT"];	
-	$cert_storate_path = __DIR__ .'/certificate/storage';
-	$path_to_keys = $cert_storate_path."/$domain";
 
 	// ###############################################
-    $le = new Analogic\ACME\Lescript($cert_storate_path, $path_to_public_html, $logger);
-    # or without $logger
-    $le->contact = ["mailto:$email"]; // optional
-    $le->initAccount();
-    $le->signDomains($domains);
+	$le = new Analogic\ACME\Lescript($cert_storate_path, $path_to_public_html, $logger);
+	# or without $logger
+	$le->contact = ["mailto:$email"]; // optional
+	$le->initAccount();
+	$le->signDomains($domains);
 
 	// needed: fullchain.pem (contains joint of "cert.pem & chain.pem") + private.pem is needed
 	mail($email, 'SSL files for : '.$domain,  "Certificate (FullChain):". PHP_EOL . PHP_EOL . file_get_contents($path_to_keys. '/fullchain.pem'). PHP_EOL . PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .PHP_EOL .	"Private Key:". PHP_EOL . PHP_EOL . PHP_EOL . file_get_contents($path_to_keys. '/private.pem'), $headers ='From: ssl_issued@'.$domain . "\r\n" .  'Reply-To: ssl_issued@'.$domain . "\r\n" .  'X-Mailer: PHP/' . phpversion() );
 	echo '<span style="font-size:2em; color:green;>KEYS SUCCESSFULLY MAILED (check your spambox)</span>'; 
 } catch (\Exception $e) {
-    $logger->error($message = $e->getMessage());
-    //$logger->error($e->getTraceAsString());  reveals server paths too...
+	$logger->error($message = $e->getMessage());
+	//$logger->error($e->getTraceAsString());  reveals server paths too...
 	mail($email, 'SSL files couldnt be created.',  "Reason:".$message , $headers ='From: ssl_issued@'.$domain . "\r\n" .  'Reply-To: ssl_issued@'.$domain . "\r\n" .  'X-Mailer: PHP/' . phpversion() );
 	echo '<span style="font-size:2em; color:green;>KEYS WERE NOT CREATED. $message</span>'; 
 }
-finally{
-	// remove traces after processing
-	if (is_dir($path_to_keys)) rmdir_recursive($path_to_keys);
-	if (is_dir($cert_storate_path)) rmdir_recursive($cert_storate_path);
-}
+
+// remove traces after processing
+if (is_dir($path_to_keys)) rmdir_recursive($path_to_keys);
+if (is_dir($cert_storate_path)) rmdir_recursive($cert_storate_path);
+
